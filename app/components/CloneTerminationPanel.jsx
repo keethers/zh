f2ea8471ec07275083c62c0316858fc3431e3833
Keethers <keithsampl20@gmail.com>
2025-07-21 02:19:11 -0700

    Create CloneTerminationPanel.jsx

---
+++ app/components/CloneTerminationPanel.jsx
@@ -0,0 +1,9 @@
+export default function CloneTerminationPanel() {
+  return (
+    <div style={{ padding: "2rem", background: "#300", color: "#fff" }}>
+      <h3>💀 Clone Termination Panel</h3>
+      <p>Manually terminate individual or entire sets of clones post-mission.</p>
+      <button style={{ backgroundColor: "darkred", color: "white" }}>Terminate All</button>
+    </div>
+  );
+}
