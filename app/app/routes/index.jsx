48e8f5189e39a0e19695367896e499f249edbceb
Keethers <keithsampl20@gmail.com>
2025-07-20 21:50:39 -0700

    Create index.jsx

---
+++ app/app/routes/index.jsx
@@ -0,0 +1,9 @@
+// app/routes/index.jsx
+export default function Index() {
+  return (
+    <div style={{ fontFamily: "system-ui", lineHeight: "1.4" }}>
+      <h1>🚀 Welcome to ExoticaEco$ystem</h1>
+      <p>This is the homepage. Your system is almost live!</p>
+    </div>
+  );
+}
