2ff20622673ceeb9d6ae16623bec3ccb7f1c4bfc
Keethers <keithsampl20@gmail.com>
2025-07-21 00:26:08 -0700

    Create root.jsx

---
+++ zh/app/app/routes/app/root.jsx
@@ -0,0 +1,27 @@
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "@remix-run/react";
+
+export default function App() {
+  return (
+    <html lang="en">
+      <head>
+        <Meta />
+        <Links />
+        <title>ExoticaEco$ystem</title>
+      </head>
+      <body>
+        <Outlet />
+        <ScrollRestoration />
+        <Scripts />
+        <LiveReload />
+      </body>
+    </html>
+  );
+
+}
