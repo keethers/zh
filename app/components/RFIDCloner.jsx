6022138a9c013b588ddcc71e3c5ce171b0743c71
Keethers <keithsampl20@gmail.com>
2025-07-21 02:04:02 -0700

    Create RFIDCloner.jsx

---
+++ app/components/RFIDCloner.jsx
@@ -0,0 +1,9 @@
+export default function RFIDCloner() {
+  return (
+    <div style={{ padding: "2rem", background: "#200", color: "#fff" }}>
+      <h3>📶 RFID Cloner</h3>
+      <p>Use your device's NFC to clone and emulate RFID access cards.</p>
+      <button>Start NFC Scan</button>
+    </div>
+  );
+}
