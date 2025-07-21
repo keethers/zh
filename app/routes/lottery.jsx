ed2ad5b39fee855e74f7aa26bc0a83c384063e01
Keethers <keithsampl20@gmail.com>
2025-07-21 01:12:14 -0700

    Create lottery.jsx

---
+++ app/routes/lottery.jsx
@@ -0,0 +1,9 @@
+export default function Lottery() {
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h2>🎟️ 5150 Lucky Draw</h2>
+      <p>Enter your numbers for a chance to win a brand new Tesla.</p>
+      <button>Pick My Numbers</button>
+    </div>
+  );
+}
