0af731d4009eda46addda91a71897b96e8ff40f7
Keethers <keithsampl20@gmail.com>
2025-07-21 01:09:25 -0700

    Create craps.jsx

---
+++ app/routes/craps.jsx
@@ -0,0 +1,9 @@
+export default function Craps() {
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h2>🎲 Craps</h2>
+      <p>Bet on the roll of the dice. High stakes, fast play.</p>
+      <button>Roll Dice</button>
+    </div>
+  );
+}
