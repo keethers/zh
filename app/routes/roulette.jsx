2f399f9fb90784c7ccfe7e57eb0be34b564647cc
Keethers <keithsampl20@gmail.com>
2025-07-21 01:10:06 -0700

    Create roulette.jsx

---
+++ app/routes/roulette.jsx
@@ -0,0 +1,9 @@
+export default function Roulette() {
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h2>🎡 Roulette</h2>
+      <p>Place your bets: red or black, odd or even, or pick your lucky number.</p>
+      <button>Spin Wheel</button>
+    </div>
+  );
+}
