e1a197f0fd391144f94ea0961f1a4fdf87d162c9
Keethers <keithsampl20@gmail.com>
2025-07-21 01:48:02 -0700

    Create exwallet-create.jsx

---
+++ app/routes/exwallet-create.jsx
@@ -0,0 +1,9 @@
+export default function EXWalletCreate() {
+  return (
+    <div style={{ padding: "2rem", background: "#111", color: "#0f0" }}>
+      <h2>🪙 Create New EX$ Wallet</h2>
+      <p>This will generate a wallet address directly inside the platform.</p>
+      <button>Create Wallet</button>
+    </div>
+  );
+}
