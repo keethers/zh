d70a0942086a3fcaf15a097443e0c35f90df9703
Keethers <keithsampl20@gmail.com>
2025-07-21 02:36:31 -0700

    Create FounderMessage.jsx

---
+++ app/components/FounderMessage.jsx
@@ -0,0 +1,11 @@
+export default function FounderMessage() {
+  return (
+    <div style={{ padding: "2rem", background: "#111", color: "#fff" }}>
+      <h3>📣 Founder’s Broadcast</h3>
+      <p>A message from Keith: “They told us not to build this. So we did.”</p>
+      <blockquote>
+        “This is for the underestimated, the outsiders, the unstoppable.”
+      </blockquote>
+    </div>
+  );
+}
