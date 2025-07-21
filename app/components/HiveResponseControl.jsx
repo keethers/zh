e6cdd86d3f56f826b72d6d5f7bc853b50f980d5b
Keethers <keithsampl20@gmail.com>
2025-07-21 01:51:48 -0700

    Create HiveResponseControl.jsx

---
+++ app/components/HiveResponseControl.jsx
@@ -0,0 +1,9 @@
+export default function HiveResponseControl() {
+  return (
+    <div style={{ padding: "2rem", background: "#220000", color: "#ff6666" }}>
+      <h3>🛸 Hive Defense Control</h3>
+      <p>Activate full hive clone swarm in response to detected threats or commands.</p>
+      <button>Trigger Hive Protocol</button>
+    </div>
+  );
+}
