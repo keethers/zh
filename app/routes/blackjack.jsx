80e277deeac8c02718ab8d5a815f5cd13ebfa51c
Keethers <keithsampl20@gmail.com>
2025-07-21 01:08:44 -0700

    Create blackjack.jsx

---
+++ app/routes/blackjack.jsx
@@ -0,0 +1,9 @@
+export default function Blackjack() {
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h2>🃏 Blackjack</h2>
+      <p>Play against the clone dealer. Try to hit 21 without busting.</p>
+      <button>Start Game</button>
+    </div>
+  );
+}
